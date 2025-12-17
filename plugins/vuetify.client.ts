a856a6ec79e4406fc2efc9b2995d88a2b3fe9a64
MarkusTratschitt <tratschitt.m@gmail.com>
2025-12-17 15:34:11 +0100

    feat: update Vuetify plugin configuration to include theme settings and remove SSR option

--- plugins/vuetify.client.ts
+++ plugins/vuetify.client.ts
@@ -3,12 +3,27 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin(nuxtApp => {
-  console.log('[vuetify] plugin loaded')
-
   const vuetify = createVuetify({
     components,
     directives,
-    ssr: false
+    theme: {
+      defaultTheme: 'dark',
+      themes: {
+        dark: {
+          dark: true,
+          colors: {
+            background: '#0E1013',
+            surface: '#12151B',
+            primary: '#2EC5FF',
+            secondary: '#5EE1FF',
+            error: '#FF4D4D',
+            info: '#2EC5FF',
+            success: '#4CAF50',
+            warning: '#FFC107'
+          }
+        }
+      }
+    }
   })
 
   nuxtApp.vueApp.use(vuetify)
