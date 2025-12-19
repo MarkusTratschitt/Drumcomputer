7f3c409e263dd866b3ac2fc7eab7f1565f3dc142
MarkusTratschitt <tratschitt.m@gmail.com>
2025-12-20 00:39:05 +0100

    feat: wrap DrumMachine in NuxtLayout for improved structure and layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <DrumMachine />
+  <NuxtLayout name="default">
+    <template #main>
+      <DrumMachine />
+    </template>
+    <template #transport>
+      <div />
+    </template>
+    <template #pads>
+      <div />
+    </template>
+    <template #drawer>
+      <div />
+    </template>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import DrumMachine from '@/components/DrumMachine.vue'
-import PadGrid from '@/components/PadGrid.vue'
-import TransportBar from '@/components/TransportBar.vue'
-import FxPanel from '@/components/panels/FxPanel.vue'
-
-import { definePageMeta } from '#imports'
-
-definePageMeta({
-  title: 'Drum Machine',
-  layout: false
-})
 </script>
